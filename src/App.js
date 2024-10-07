3d1a0f12800fc6d3275833b6e9e69e76d6eca30b
Trisha Patel <trishap0104@gmail.com>
2024-10-06 22:09:36 -0400

    .gitignore added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: StudentID || Student Name || DevOps</h5>
+      <h5>Developed By: 101412123 || Trisha Patel || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
