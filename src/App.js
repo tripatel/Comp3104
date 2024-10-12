35416ecd91b659b1d5fadc9783335606fecead1b
Trisha Patel <trishap0104@gmail.com>
2024-10-12 14:05:45 -0400

    title changed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101412123 || Trisha Patel || DevOps</h5>
+      <h5>Developed By: 101412123 || Trisha Patel || DevOps - COMP3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
